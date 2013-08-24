0bb3fbe3aff6087792d66dc8ac44c7c3195cd148
carlcarl <carlcarlking@gmail.com>
2013-08-24 17:05:24 +0800

    Append '繼續閱讀' after each article

--- javascript/app.js
+++ javascript/app.js
@@ -39,18 +39,19 @@ angular.module("kde.linux.org.tw", ['firebase']).factory({
   	return null;
   })
 }).filter('less', function() {
-  return function(str) {
-    var times = 2, i, pos = 0, tmp, endTag = '</p>';
+  return function(str, link) {
+    var times = 2, i, pos = 0, tmp, endTag = '</p>', linkTag;
+    linkTag = '<a href="' + link + '">[繼續閱讀......]</a>';
     for(i = 0; i < times; i += 1, pos = tmp) {
       tmp = str.indexOf(endTag, pos + 1);
       if(tmp == -1) {
         if(pos != 0) {
           break;
         }
-        return str;
+        return str + linkTag;
       }
     }
-    return str.substring(0, pos + endTag.length);
+    return str.substring(0, pos + endTag.length) + linkTag;
   };
 });
 show = function(){
