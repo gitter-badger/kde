70febdddefd496193fecf6a65ac9dd8ae80c930f
Franklin <franklin@goodhorse.idv.tw>
2013-10-22 07:38:12 +0800

    Adding some more log to deploy.sh

--- GitHubHook/deploy.sh
+++ GitHubHook/deploy.sh
@@ -3,6 +3,13 @@
 cd $1
 echo "cd $1 to pull $2" >> /tmp/deploy.log
 git pull origin $2
+RES=$?
+echo "git result: $RES" >> /tmp/deploy.log
 /usr/local/bin/grunt deploy
+RES=$?
+echo "grunt result: $RES" >> /tmp/deploy.log
 rm -rf ./_site/*
+echo "_site cleaned." >> /tmp/deploy.log
 /usr/local/bin/jekyll build
+RES=$?
+echo "jekyll result: $RES" >> /tmp/deploy.log
