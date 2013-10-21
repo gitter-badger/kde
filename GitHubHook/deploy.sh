51e6891a2858d77d82211902c7cbff96bfc38c12
Franklin <franklin@goodhorse.idv.tw>
2013-10-22 07:45:14 +0800

    Fine tuning deploy.sh

--- GitHubHook/deploy.sh
+++ GitHubHook/deploy.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
-cd $1
-echo "cd $1 to pull $2" >> /tmp/deploy.log
-git pull origin $2
+GRUNT=/usr/local/bin/grunt
+JEKYLL=/usr/local/bin/jekyll
+
+if [ -z "$1" ]; then
+	# default path
+	TARGET=/opt/KDE-Taiwan/kde
+else
+	TARGET=$1
+fi
+
+if [ -z "$2" ]; then
+	# default branch to master
+	BRANCH=master
+else
+	BRANCH=$2
+fi
+
+cd $TARGET
+echo "cd $TARGET to pull $BRANCH" >> /tmp/deploy.log
+git pull origin $BRANCH
 RES=$?
 echo "git result: $RES" >> /tmp/deploy.log
-/usr/local/bin/grunt deploy
+$GRUNT deploy
 RES=$?
 echo "grunt result: $RES" >> /tmp/deploy.log
 rm -rf ./_site/*
 echo "_site cleaned." >> /tmp/deploy.log
-/usr/local/bin/jekyll build
+$JEKYLL build
 RES=$?
 echo "jekyll result: $RES" >> /tmp/deploy.log
